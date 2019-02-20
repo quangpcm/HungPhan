607aa59ed04e8e49ef68b316af3b0f2fd1c0a66b
Quang Phu C M <phuchiminhquang@gmail.com>
2019-02-20 08:54:40 +0700

    Adding new profile

---
+++ BasicSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BasicSwift
+//
+//  Created by Quang Phu C M on 2/19/19.
+//  Copyright © 2019 Quang Phu C M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
